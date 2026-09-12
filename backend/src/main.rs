8191420d5c6ae4263c8f52b71c9f17e2a2d59ffb
Oliver Piorun <ollipio@gmail.com>
2026-09-13 00:22:17 +0200

    Improve the wording

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,7 +3,7 @@ use air_compass::models::OutdoorWeather;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     air_compass::run(|| async {
-        // Use your favorite weather API or source for the outdoor weather
+        // Use your preferred weather API or source for the outdoor weather
         Ok(OutdoorWeather {
             temperature: 0.0,
             relative_humidity: 0.0,
